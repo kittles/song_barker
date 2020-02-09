06129dea7c1b7520311ae5cce75fe1d0afe9a3c8
Patrick Brooks <pat.w.brooks@gmail.com>
2020-02-08 17:19:34 -0800

    initialize python env

--- initial_setup.sh
+++ initial_setup.sh
@@ -1,2 +1,12 @@
+# python env
+sudo apt install virtualenv
+cd audio_processing
+virtualenv -p python3 .env
+source .env/bin/activate
+pip install -r requirements.txt
+cd ..
+
+# initialize database
 cd server
 node -e 'require("./database.js").initialize_db()'
+
