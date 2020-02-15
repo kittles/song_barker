22278f8f7cb5379bb7d8089a9cf3a49a628c3bb5
Patrick Brooks <pat.w.brooks@gmail.com>
2020-02-15 13:12:15 -0800

    more deploy scripts

--- deploy_dev.sh
+++ deploy_dev.sh
@@ -3,3 +3,5 @@ git pull origin master
 cd server
 pm2 restart index.js
 # TODO notify discord
+discord_webhook_url="https://discordapp.com/api/webhooks/677265855716786196/Gp3WPXSRtR4UivRy6H3_3FXyRNjUvWPceAC0M5mhRJ2DJuHGlerEK2PO9RI7hojpG-sM"
+curl -H "Content-Type: application/json" -X POST -d '{"username": "dev server bot", "content": "dev server deployed 😎"}' $discord_webhook_url
