3492abdba6f7fa985409f664dc8c93bc213982ea
Patrick Brooks <pat.w.brooks@gmail.com>
2020-05-14 12:14:35 -0700

    ffmpeg force overwrite

--- audio_processing/audio_conversion.py
+++ audio_processing/audio_conversion.py
@@ -5,14 +5,14 @@ import warnings
 def aac_to_wav (aac_fp):
     with warnings.catch_warnings():
         wav_fp = aac_fp.replace('.aac', '.wav')
-        sp.call('ffmpeg -nostats -hide_banner -loglevel panic  -i {} {}'.format(aac_fp, wav_fp), shell=True)
+        sp.call('ffmpeg -nostats -hide_banner -loglevel panic -y -i {} {}'.format(aac_fp, wav_fp), shell=True)
     return wav_fp
 
 
 def wav_to_aac (wav_fp):
     with warnings.catch_warnings():
         aac_fp = wav_fp.replace('.wav', '.aac')
-        sp.call('ffmpeg -nostats -hide_banner -loglevel panic  -i {} {}'.format(wav_fp, aac_fp), shell=True)
+        sp.call('ffmpeg -nostats -hide_banner -loglevel panic -y -i {} {}'.format(wav_fp, aac_fp), shell=True)
     return aac_fp
 
 
