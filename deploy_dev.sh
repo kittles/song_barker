e83c68f7361663981b54db8dee17edb89f89b3c5
Patrick Brooks <pat.w.brooks@gmail.com>
2020-06-08 14:12:06 -0700

    deploy script modified

--- deploy_dev.sh
+++ deploy_dev.sh
@@ -1,18 +1,18 @@
 git stash
 git pull origin master
-if [ "$1" == "q" ]; then
-    cd server
-    pm2 restart app.js
-    discord_webhook_url="https://discordapp.com/api/webhooks/692090733842268221/xVUsUktputk7B7ePZsC3jx9ltlk3ffZ8OwVmSxZagczK0c1htUu-IweCS1JmAsdb3ZXn"
-    curl -H "Content-Type: application/json" -X POST -d '{"username": "dev server bot", "content": "\n ********** \n 😎 dEv SeRveR dEPlOyEd 😎 \n ********** \n"}' $discord_webhook_url
-else
-    cd audio_processing
+
+# update env and node_modules
+if [ "$1" == "u" ]; then
+    cd /home/patrick/song_barker/audio_processing
     source .env/bin/activate
     pip install -r requirements.txt
     deactivate
-    cd ../server
+    cd /home/patrick/song_barker/server
     npm install
-    pm2 restart app.js
-    discord_webhook_url="https://discordapp.com/api/webhooks/692090733842268221/xVUsUktputk7B7ePZsC3jx9ltlk3ffZ8OwVmSxZagczK0c1htUu-IweCS1JmAsdb3ZXn"
-    curl -H "Content-Type: application/json" -X POST -d '{"username": "dev server bot", "content": "\n ********** \n 😎 dEv SeRveR dEPlOyEd 😎 \n ********** \n"}' $discord_webhook_url
 fi
+
+# restart express server
+cd /home/patrick/song_barker/server
+pm2 restart app.js
+discord_webhook_url="https://discordapp.com/api/webhooks/692090733842268221/xVUsUktputk7B7ePZsC3jx9ltlk3ffZ8OwVmSxZagczK0c1htUu-IweCS1JmAsdb3ZXn"
+curl -H "Content-Type: application/json" -X POST -d '{"username": "dev server bot", "content": "\n ********** \n 😎 dEv SeRveR dEPlOyEd 😎 \n ********** \n"}' $discord_webhook_url
