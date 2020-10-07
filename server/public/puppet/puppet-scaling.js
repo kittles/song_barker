2f3cbecba19a046a8f5f45dcb2e6cc2c51b65120
Patrick Brooks <pat.w.brooks@gmail.com>
2020-10-07 11:04:29 -0700

    card sizing

--- server/public/puppet/puppet-scaling.js
+++ server/public/puppet/puppet-scaling.js
@@ -1,7 +1,10 @@
 var initial_viewport_height = window.innerHeight;
 $(document).ready(() => {
+    function wide_mode () {
+        return document.body.offsetWidth > document.body.offsetHeight;
+    }
     function set_container_scale () {
-        var content_height = 1308.84
+        var content_height = wide_mode() ? 1095 : 1158.84;
         var viewport_height = Math.min(window.innerHeight, initial_viewport_height);
         $('#container').css({
             transform: `scale(${viewport_height / content_height})`,
