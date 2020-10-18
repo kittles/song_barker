81750f1890e29967de5ae4c7ff9b64776e0bd1d5
kittles <pat.w.brooks@gmail.com>
2020-10-18 03:36:21 +0000

    env vars for prod

--- config.sh
+++ config.sh
@@ -1,13 +1,13 @@
 export k9_dev=False
-export k9_domain_name="k9karaoke.com"
+export k9_domain_name="k-9karaoke.com"
 export k9_ip_address=68.183.113.8
-export k9_bucket_name="k9karaoke-bucket"
+export k9_bucket_name="k9karaoke-prod"
 export k9_project_root="/home/patrick/song_barker"
 
 export k9_credentials_dir=$k9_project_root"/credentials"
 export k9_audio_dir=$k9_project_root"/audio_processing"
 export k9_server_dir=$k9_project_root"/server"
-export k9_database=$k9_server_dir"/barker_database.db"
+export k9_database=$k9_server_dir"/k9karaoke-database-prod.db"
 
 # this is for bucket upload etc
 export GOOGLE_APPLICATION_CREDENTIALS=$k9_credentials_dir"/bucket-credentials.json"
