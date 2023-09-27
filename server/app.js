78ff27e16555619d8d20fc398ac2acac7dba696a
turboblasterunlimited <66650587+turboblasterunlimited@users.noreply.github.com>
2023-09-27 15:54:03 -0700

    adding logging

--- server/app.js
+++ server/app.js
@@ -211,17 +211,17 @@ app.post('/revenuecat', async(req, res) => {
 app.post('/appstore', async(req, res) => {
     res.status(200).send();
     console.log("/appstore: Sent 200 code");
-    // try {
-    //     console.log("event_type:", req.body.event.type);
-    //     //console.log(JSON.stringify(req.body));
-    //     var event = rcat.translate_event(req.body.event);
-    //     console.log("about to send event");
-    //     var result = await send_revenuecat_ga4(event);
-    //     console.log("revenue cat GA4 send result:", result);
-    // }
-    // catch (err) {
-    //     console.log("Error parsing JSON string:", err);
-    // }
+    try {
+        console.log(responseBodyV2);
+        // //console.log(JSON.stringify(req.body));
+        // var event = rcat.translate_event(req.body.event);
+        // console.log("about to send event");
+        // var result = await send_revenuecat_ga4(event);
+        // console.log("revenue cat GA4 send result:", result);
+    }
+    catch (err) {
+        console.log("Error parsing JWS string:", err);
+    }
     return;
 });
 
